8fb024e52b8c3e0f4ffd9212f334026aa95e8636
muink <hukk1996@gmail.com>
2018-09-11 10:44:19 +0800

    应用 Research Queue 本地化语言设置

---
+++ settings-updates.lua
@@ -0,0 +1,12 @@
+
+--research queue settings
+if mods["research-queue"] then
+	data.raw["bool-setting"]["research-queue_popup-on-queue-finish"].localised_name = nil
+	data.raw["bool-setting"]["research-queue_popup-on-queue-finish"].localised_description = nil
+	data.raw["int-setting"]["research-queue-rows-count"].localised_name = nil
+	data.raw["int-setting"]["research-queue-rows-count"].localised_description = nil
+	data.raw["int-setting"]["research-queue-table-width"].localised_name = nil
+	data.raw["int-setting"]["research-queue-table-width"].localised_description = nil
+	data.raw["int-setting"]["research-queue-table-height"].localised_name = nil
+	data.raw["int-setting"]["research-queue-table-height"].localised_description = nil
+end
