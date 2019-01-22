d1d05603e874185d11e0a36d3b7a3f2d434e6753
muink <hukk1996@gmail.com>
2019-01-22 17:11:49 +0800

    修补 PickerExtended 版本验证

---
+++ lualib/functions.lua
@@ -0,0 +1,15 @@
+
+--Compare mods version
+function compare_version(ReferVersion, CurrentVersion)
+	local r={}
+	local v={}
+	_, _, r[1], r[2], r[3] = string.find(ReferVersion, "(%d+)%.(%d+)%.(%d+)")
+	_, _, v[1], v[2], v[3] = string.find(CurrentVersion, "(%d+)%.(%d+)%.(%d+)")
+	local status
+	for i=1,3,1 do
+		if v[i]-r[i] > 0 then status=1; break
+		elseif v[i]-r[i] < 0 then status=-1; break
+		else status=0 end
+	end
+	return status
+end
