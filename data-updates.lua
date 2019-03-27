a9749d9d310bf02b29be8f7ac09f949f380b428b
muink <hukk1996@gmail.com>
2019-03-27 10:52:54 +0800

    移除 PickerExtended 中的无用代码

--- data-updates.lua
+++
@@ -1,6 +0,0 @@
-
---PickerExtended items
-if mods["PickerExtended"] and compare_version(mods["PickerExtended"], "3.0.3") > 0 then
-	data.raw["selection-tool"]["picker-dolly"].localised_name = nil
-	data.raw["selection-tool"]["picker-dolly"].localised_description = nil
-end
